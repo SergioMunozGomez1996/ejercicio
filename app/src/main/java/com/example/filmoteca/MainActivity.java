44ad8fe86f5d6517f7cb01cdf46055bb0eac7186
mastermoviles <smg158@alu.ua.es>
2019-10-01 17:21:35 +0200

    Añadido Log en onStart

--- app/src/main/java/com/example/filmoteca/MainActivity.java
+++ app/src/main/java/com/example/filmoteca/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.filmoteca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String LOG = "DebugApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG, "onStart");
     }
 }
